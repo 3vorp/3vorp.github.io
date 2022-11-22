ec22ed6fa84c785d4b0db8fbbabfbda3613738ad
Evorp <3vorpgaming@gmail.com>
2022-11-22 12:20:14 -0800

    Updates

--- _app/immutable/chunks/1-3a06e8ff.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../components/error.svelte-56f1c82f.js";export{t as component};
---
+++ _app/immutable/components/pages/_page.svelte-40db5320.js
@@ -0,0 +1,5 @@
+import{S as E,i as q,s as B,k as n,q as h,a as S,l as m,m as v,r as c,h as l,c as k,n as C,b,G as r,B as x}from"../../chunks/index-edd250b0.js";import{b as D}from"../../chunks/paths-6cd3a76e.js";/* empty css                       */function G(A){let t,i,f,p,u,_,o,s,d;return{c(){t=n("div"),i=n("h1"),f=h("hello"),p=h(`
+    trolling`),u=n("br"),_=h(`
+    very gaming`),o=S(),s=n("a"),d=h("About"),this.h()},l(e){t=m(e,"DIV",{});var a=v(t);i=m(a,"H1",{});var g=v(i);f=c(g,"hello"),g.forEach(l),p=c(a,`
+    trolling`),u=m(a,"BR",{}),_=c(a,`
+    very gaming`),a.forEach(l),o=k(e),s=m(e,"A",{href:!0});var y=v(s);d=c(y,"About"),y.forEach(l),this.h()},h(){C(s,"href",D+"/about")},m(e,a){b(e,t,a),r(t,i),r(i,f),r(t,p),r(t,u),r(t,_),b(e,o,a),b(e,s,a),r(s,d)},p:x,i:x,o:x,d(e){e&&l(t),e&&l(o),e&&l(s)}}}class R extends E{constructor(t){super(),q(this,t,null,G,B,{})}}export{R as default};
