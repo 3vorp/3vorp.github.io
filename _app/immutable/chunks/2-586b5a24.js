6d7f66320dfd94311958d2d43f249b2e5cfccd22
Evorp <3vorpgaming@gmail.com>
2022-12-23 22:44:33 -0800

    Updates

--- _app/immutable/chunks/2-586b5a24.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../components/pages/_page.svelte-87e033ab.js";export{t as component};
--- _app/immutable/chunks/3-b617e9ab.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../components/pages/about/_page.svelte-6c70378e.js";export{t as component};
