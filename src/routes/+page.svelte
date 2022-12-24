1536f68f689e84b1aae6b47e2b62babdd0afa0fd
Evorp <3vorpgaming@gmail.com>
2022-12-23 23:20:27 -0800

    linear-gradient and font-weight my beloved

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -7,31 +7,39 @@
 </svelte:head>
 
 <div>
-    <h1>What I do:</h1>
-
-    <h2>I work on the Faithful Project as a co-owner/manager...</h2>
-    Our main site is at <a href="https://faithfulpack.net">https://faithfulpack.net</a> and our discord at <a href="https://discord.gg/sN9YRQbBv7">https://discord.gg/sN9YRQbBv7</a>.
-
-    <h2>I made a discord bot in discord.py and it only slightly sucks...</h2>
-    You can check it out at <a href="https://github.com/3vorp/Spunch-Bot">https://github.com/3vorp/Spunch-Bot</a>.
-
-    <h2>I do commissions for pixel art...</h2>
-    I mostly do basic small-scale art like Minecraft skins — usually costs between 5-10 USD depending on complexity.
-
+    <img src="banner.png" alt="evorp banner" class="banner_img">
 </div>
 
-<br><hr>
+<div class="black_gradient">
+    <div class="padding_div">
+        <div>
+            <h1>What I do:</h1>
 
-<div>
-    <h1>Contact me:</h1>
+            <h2>I work on the Faithful Project as a co-owner/manager...</h2>
+            Our main site is at <a href="https://faithfulpack.net">https://faithfulpack.net</a> and our discord at <a href="https://discord.gg/sN9YRQbBv7">https://discord.gg/sN9YRQbBv7</a>.
 
-    <h2>The best way to contact me is via Discord at <code>Evorp#5819</code>. I should get back to you within a few hours depending on the time.</h2>
+            <h2>I made a discord bot in discord.py and it only slightly sucks...</h2>
+            You can check it out at <a href="https://github.com/3vorp/Spunch-Bot">https://github.com/3vorp/Spunch-Bot</a>.
 
-    Alternatively you can find me at:
-    <br><br>
-    <a href="https://twitter.com/3vorp">Twitter</a> • <a href="https://reddit.com/u/3vorp">Reddit</a> • <a href="https://www.youtube.com/@evorp">YouTube</a> • <a href="https://steamcommunity.com/id/3vorp/">Steam</a>
+            <h2>I do commissions for pixel art...</h2>
+            I mostly do basic small-scale art like Minecraft skins — usually costs between 5-10 USD depending on complexity.
+
+        </div>
+
+        <br><hr>
+
+        <div>
+            <h1>Contact me:</h1>
+
+            <h2>The best way to contact me is via Discord at <code>Evorp#5819</code>. I should get back to you within a few hours depending on the time.</h2>
+
+            Alternatively you can find me at:
+            <br><br>
+            <a href="https://twitter.com/3vorp">Twitter</a> • <a href="https://reddit.com/u/3vorp">Reddit</a> • <a href="https://www.youtube.com/@evorp">YouTube</a> • <a href="https://steamcommunity.com/id/3vorp/">Steam</a>
+        </div>
+
+        <br><hr>
+
+        <h2>More coming soon...</h2>
+    </div>
 </div>
-
-<br><hr>
-
-<h2>More coming soon...</h2>
