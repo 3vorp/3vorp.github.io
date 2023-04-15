f26adefee430588f7e408a680835c6d59dd3da04
Evorp <3vorpgaming@gmail.com>
2023-04-14 22:27:53 -0700

    add gambling because why not at this point

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -15,13 +15,13 @@
         <h1>What I do:</h1>
 
         <h2>I work on the Faithful Project as a co-owner/manager...</h2>
-        Our main site is at <a href="https://faithfulpack.net">https://faithfulpack.net</a> and our discord at <a href="https://discord.gg/sN9YRQbBv7">https://discord.gg/sN9YRQbBv7</a>.
+        <p>Our main site is at <a href="https://faithfulpack.net">https://faithfulpack.net</a> and our discord at <a href="https://discord.gg/sN9YRQbBv7">https://discord.gg/sN9YRQbBv7</a>.</p>
 
         <h2>I made a discord bot in discord.py and it only slightly sucks...</h2>
-        You can check it out at <a href="https://github.com/3vorp/Spunch-Bot">https://github.com/3vorp/Spunch-Bot</a>.
+        <p>You can check it out at <a href="https://github.com/3vorp/Spunch-Bot">https://github.com/3vorp/Spunch-Bot</a>.</p>
 
         <h2>I do commissions for pixel art...</h2>
-        I mostly do basic small-scale art like Minecraft skins — usually costs between 5-10 USD depending on complexity.
+        <p>I mostly do basic small-scale art like Minecraft skins — usually costs between 5-10 USD depending on complexity.</p>
 
     </div>
 
@@ -32,12 +32,11 @@
 
         <h2>The best way to contact me is via Discord at <code>Evorp#5819</code>. I should get back to you within a few hours depending on the time.</h2>
 
-        Alternatively you can find me at:
-        <br><br>
-        <a href="https://github.com/3vorp">GitHub</a> • <a href="https://twitter.com/3vorp">Twitter</a> • <a href="https://reddit.com/u/3vorp">Reddit</a> • <a href="https://www.youtube.com/@evorp">YouTube</a> • <a href="https://steamcommunity.com/id/3vorp/">Steam</a>
+        <p>Alternatively you can find me at:</p>
+        <p><a href="https://github.com/3vorp">GitHub</a> • <a href="https://twitter.com/3vorp">Twitter</a> • <a href="https://reddit.com/u/3vorp">Reddit</a> • <a href="https://www.youtube.com/@evorp">YouTube</a> • <a href="https://steamcommunity.com/id/3vorp/">Steam</a></p>
     </div>
 
     <br><hr>
 
-    <h2>More coming soon...</h2>
+    <p><a href ="/gamble">Gamble your savings away until I add more stuff to this site!</a></p>
 </div>
---
+++ src/routes/gamble/+page.svelte
@@ -0,0 +1,88 @@
+<script>
+    import "../../css/main.scss";
+    const randint = (start, stop) =>
+        start + Math.floor(Math.random() * (stop - start));
+
+    let integer = 69; // nice
+    function generateRandomNumber() {
+        integer = randint(0, 100);
+    }
+</script>
+
+<svelte:head>
+    <title>Evorp's Website</title>
+</svelte:head>
+
+<p>
+    I do not take responsibility for any losses you may suffer as a result of
+    this page.
+</p>
+<div id="wrapper">
+    <h1>Gambling Simulator</h1>
+    <button on:click={generateRandomNumber}>
+        Definitely random number: {integer}
+    </button>
+    <h2>
+        Press the above text to generate a new number and fund your gambling
+        addiction!
+    </h2>
+</div>
+<div id="home">
+    <hr>
+    <h3>
+        <a href="/">Return to Home Page</a>
+    </h3>
+</div>
+
+<style>
+    * {
+        text-align: center;
+    }
+
+    h1 {
+        margin-top: 0px;
+        margin-bottom: 50px;
+        font-size: 4.5em;
+        font-weight: bold;
+    }
+
+    h2 {
+        margin-top: 50px;
+        margin-bottom: 0px;
+        font-size: 1.5em;
+        font-weight: normal;
+    }
+
+    h3 {
+        font-weight: lighter;
+        font-size: 1.5em;
+    }
+
+    button {
+        font-size: 2.5em;
+        background: transparent;
+        border: none;
+        color: #dedede;
+    }
+
+    p {
+        font-size: 0.75em;
+        position: relative;
+    }
+
+    #wrapper {
+        position: absolute;
+        left: 50%;
+        top: 50%;
+        -webkit-transform: translate(-50%, -50%);
+        transform: translate(-50%, -50%);
+    }
+
+    #home {
+        position: absolute;
+        top: 90%;
+        left: 50%;
+        -webkit-transform: translate(-50%, -10%);
+        transform: translate(-50%, -10%);
+    }
+</style>
