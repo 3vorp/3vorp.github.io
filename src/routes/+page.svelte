f9e82ce8254e120cf0a6736e8e2ffe181555a2db
Evorp <3vorpgaming@gmail.com>
2023-05-16 18:16:35 -0700

    reflect changes to github readme, also add some effects

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -11,32 +11,77 @@
 </div>
 
 <div class="padding_div">
-    <div>
-        <h1>What I do:</h1>
+    <h1>Welcome to my website!</h1>
 
-        <h2>I work on the Faithful Project as a co-owner/manager...</h2>
-        <p>Our main site is at <a href="https://faithfulpack.net">https://faithfulpack.net</a> and our discord at <a href="https://discord.gg/sN9YRQbBv7">https://discord.gg/sN9YRQbBv7</a>.</p>
+    <p>I'm a pixel artist and programmer with experience in Python, JavaScript/TypeScript in Node, web development, and more recently Java and the SvelteKit website framework.</p>
+    <p>The best way to contact me is via Discord at <code>Evorp#5819</code>, but you can also find me at the following links:</p>
+    <p>
+        <a href="https://github.com/3vorp">GitHub</a> •
+        <a href="https://twitter.com/3vorp">Twitter</a> •
+        <a href="https://reddit.com/u/3vorp">Reddit</a> •
+        <a href="https://www.youtube.com/@evorp">YouTube</a> •
+        <a href="https://steamcommunity.com/id/3vorp/">Steam</a>
+    </p>
+    <h1>What I do:</h1>
 
-        <h2>I made a discord bot in discord.py and it only slightly sucks...</h2>
-        <p>You can check it out at <a href="https://github.com/3vorp/Spunch-Bot">https://github.com/3vorp/Spunch-Bot</a>.</p>
+    <h2 align="center">The Faithful Project</h2>
+    <a href="https://faithfulpack.net">
+    <img
+        src="https://github.com/Faithful-Resource-Pack/Branding/blob/main/logos/transparent/512/plain_logo.png?raw=true"
+        alt="faithful logo"
+        height="320"
+        class="left-align hoverable"
+    />
+    </a>
 
-        <h2>I do commissions for pixel art...</h2>
-        <p>I mostly do basic small-scale art like Minecraft skins — usually costs between 5-10 USD depending on complexity.</p>
+    <p>
+    Faithful is a collection of open source and community-driven Minecraft resource packs, aiming to provide a higher-resolution version of the default Minecraft textures while staying true to the originals.
+    <br><br>
+    I'm a co-owner and manager of the project, and have been involved for over two years in its development. I help with <a href="https://github.com/Faithful-Resource-Pack/Website">website</a> development, <a href="https://github.com/Faithful-Resource-Pack/Discord-Bot">Discord bot</a> development, <a href="https://docs.faithfulpack.net/pages/textures/contributor-handbook">texture making</a>, and much more.
+    <br><br>
+    You can join our <a href="https://discord.gg/sN9YRQbBv7">Discord server</a> to help us contribute to the project, give feedback, or just generally chat with us, or click the image on the left to view our website.
+    </p>
 
-    </div>
+    <h2 align="center">spunch bot</h2>
+    <a href="https://github.com/3vorp/spunch-bot">
+    <img
+        src="https://github.com/3vorp/Spunch-Bot/blob/main/assets/bot_pfp.png?raw=true"
+        alt="spunch bot pfp"
+        height="320"
+        class="right-align hoverable"
+    />
+    </a>
 
-    <br><hr>
+    <p>
+    spunch bot is a general-purpose Discord bot, featuring per-server custom prefixes, an intuitive feedback ticket system, global announcements from a customizable whitelist, and much more.
+    <br><br>
+    Started in October 2022 as a hobby project of my own using the <a href="https://discordpy.readthedocs.io/">discord.py</a> framework, mostly as a joke, anyone can now run it by simply forking the repository and following the instructions in the README.
+    <br><br>
+    You can click the image on the right to get started with cloning and installing the bot anywhere.
+    </p>
 
-    <div>
-        <h1>Contact me:</h1>
+    <br>
+    <h2 align="center">Commissions</h2>
 
-        <h2>The best way to contact me is via Discord at <code>Evorp#5819</code>. I should get back to you within a few hours depending on the time.</h2>
+    <p><a href="https://discord.com/users/360249987927638016">
+    <img
+        src="https://cdn.discordapp.com/attachments/931741283179954187/1108194846499414076/zarrinHead.png"
+        alt="commission example"
+        height="320"
+        class="left-align hoverable"
+    />
+    </a></p>
+    <p>
+    I do commissions for small-scale pixel art work, such as Minecraft skins, textures, and profile pictures. They'll generally cost between 5-10 USD, depending on complexity and time required.
+    <br><br>
+    I have well over two years of pixel art and graphic design experience under my belt, and have been doing commission work for nearly one and a half years. I prioritize client feedback above all and ensure maximum transparency throughout the designing process.
+    <br><br>
+    If you are interested in commissioning me, please let me know via Discord by clicking the link on the left and sending a friend request. We can then proceed in direct messages.
+    </p>
 
-        <p>Alternatively you can find me at:</p>
-        <p><a href="https://github.com/3vorp">GitHub</a> • <a href="https://twitter.com/3vorp">Twitter</a> • <a href="https://reddit.com/u/3vorp">Reddit</a> • <a href="https://www.youtube.com/@evorp">YouTube</a> • <a href="https://steamcommunity.com/id/3vorp/">Steam</a></p>
-    </div>
+    <hr>
 
-    <br><hr>
+    <p><em>Please contact me via Discord if you have any additional questions. You can use my other socials above but your response times may vary.</em></p>
 
-    <p><a href ="/gamble">Gamble your savings away until I add more stuff to this site!</a></p>
+    <p align="center"><a href="/gamble">Gamble your savings away until I add more stuff to this site!</a></p>
 </div>
