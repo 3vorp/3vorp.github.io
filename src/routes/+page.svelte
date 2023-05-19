9345077d04875abb2b7ad5821965e9840290420c
Evorp <3vorpgaming@gmail.com>
2023-05-18 21:14:10 -0700

    add banners and fix up home page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,15 +3,12 @@
 <div class="padding-div">
     <h1>Welcome to my website!</h1>
 
-    <p>I'm a pixel artist and programmer with experience in Python, JavaScript/TypeScript in Node, web development, and more recently Java and the SvelteKit website framework.</p>
-    <p>The best way to contact me is via Discord at <code>Evorp#5819</code>, but you can also find me at the following links:</p>
     <p>
-        <a href="https://github.com/3vorp">GitHub</a> •
-        <a href="https://twitter.com/3vorp">Twitter</a> •
-        <a href="https://reddit.com/u/3vorp">Reddit</a> •
-        <a href="https://www.youtube.com/@evorp">YouTube</a> •
-        <a href="https://steamcommunity.com/id/3vorp/">Steam</a>
+        I'm a pixel artist and programmer with experience in Python, JavaScript/TypeScript in Node, web development, and more recently Java and the SvelteKit website framework.
+        <br><br>
+        The best way to contact me is via Discord at <code>Evorp#5819</code>, but you can also find me at the links in the footer too.
     </p>
+
     <h1>What I do:</h1>
 
     <h2 align="center">The Faithful Project</h2>
@@ -25,11 +22,11 @@
     </a>
 
     <p>
-    Faithful is a collection of open source and community-driven Minecraft resource packs, aiming to provide a higher-resolution version of the default Minecraft textures while staying true to the originals.
-    <br><br>
-    I'm a co-owner and manager of the project, and have been involved for over two years in its development. I help with <a href="https://github.com/Faithful-Resource-Pack/Website">website</a> development, <a href="https://github.com/Faithful-Resource-Pack/Discord-Bot">Discord bot</a> development, <a href="https://docs.faithfulpack.net/pages/textures/contributor-handbook">texture making</a>, and much more.
-    <br><br>
-    Click the image on the left to see more information about Faithful as a whole, and join our <a href="https://discord.gg/sN9YRQbBv7">Discord server</a> to help us contribute to the project, give feedback, or just generally chat with us!
+        Faithful is a collection of open source and community-driven Minecraft resource packs, aiming to provide a higher-resolution version of the default Minecraft textures while staying true to the originals.
+        <br><br>
+        I'm a co-owner and manager of the project, and have been involved for over two years in its development. I help with <a href="https://github.com/Faithful-Resource-Pack/Website" target="_blank" rel="noopener noreferrer">website</a> development, <a href="https://github.com/Faithful-Resource-Pack/Discord-Bot" target="_blank" rel="noopener noreferrer">Discord bot</a> development, <a href="https://docs.faithfulpack.net/pages/textures/contributor-handbook" target="_blank" rel="noopener noreferrer">texture making</a>, and much more.
+        <br><br>
+        Click the image on the left to see more information about Faithful as a whole, and join our <a href="https://discord.gg/sN9YRQbBv7" target="_blank" rel="noopener noreferrer">Discord server</a> to help us contribute to the project, give feedback, or just generally chat with us!
     </p>
 
     <h2 align="center">spunch bot</h2>
@@ -43,35 +40,38 @@
     </a>
 
     <p>
-    spunch bot is a general-purpose Discord bot, featuring per-server custom prefixes, an intuitive feedback ticket system, global announcements from a customizable whitelist, and much more.
-    <br><br>
-    Started in October 2022 as a hobby project of my own using the <a href="https://discordpy.readthedocs.io/">discord.py</a> framework, mostly as a joke, anyone can now run it by simply forking the repository and following the instructions in the README.
-    <br><br>
-    Click the image on the right to see more information about running, using, and modifying this bot!
+        spunch bot is a general-purpose Discord bot, featuring per-server custom prefixes, an intuitive feedback ticket system, global announcements from a customizable whitelist, and much more.
+        <br><br>
+        Started in October 2022 as a hobby project of my own using the <a href="https://discordpy.readthedocs.io/">discord.py</a> framework, mostly as a joke, anyone can now run it by simply forking the repository and following the instructions in the README.
+        <br><br>
+        Click the image on the right to see more information about running, using, and modifying this bot!
     </p>
 
     <br>
     <h2 align="center">Commissions</h2>
 
-    <p><a href="/commissions">
+    <a href="/commissions">
     <img
         src="https://cdn.discordapp.com/attachments/931741283179954187/1108194846499414076/zarrinHead.png"
         alt="commission example"
         height="320"
         class="left-align hoverable"
     />
-    </a></p>
+    </a>
+
     <p>
-    I do commissions for small-scale pixel art work, such as Minecraft skins, textures, and profile pictures. They'll generally cost between 5-10 USD, depending on complexity and time required.
-    <br><br>
-    I have well over two years of pixel art and graphic design experience under my belt, and have been doing commission work for nearly one and a half years. I prioritize client feedback above all and ensure maximum transparency throughout the designing process.
-    <br><br>
-    If you are interested in commissioning me, click the image on the left for more information.
+        I do commissions for small-scale pixel art work, such as Minecraft skins, textures, and profile pictures. They'll generally cost between 5-10 USD, depending on complexity and time required.
+        <br><br>
+        I have well over two years of pixel art and graphic design experience under my belt, and have been doing commission work for nearly one and a half years. I prioritize client feedback above all and ensure maximum transparency throughout the designing process.
+        <br><br>
+        If you are interested in commissioning me, click the image on the left for more information.
     </p>
 
     <hr>
 
-    <p><em>Please contact me via Discord if you have any additional questions. You can use my other socials too, but my response times may vary.</em></p>
+    <p><em>
+        Please contact me via <a href="https://discord.com/users/360249987927638016" target="_blank" rel="noopener noreferrer">Discord</a> if you have any additional questions. You can use my other socials too, but my response times may vary.
+    </em></p>
     <br>
-    <p align="center"><a href="/gamble">Gamble your savings away until I add more stuff to this site!</a></p>
+    <a href="/gamble" align="center">Gamble your savings away until I add more stuff to this site!</a>
 </div>
--- src/routes/commissions/+page.svelte
+++ src/routes/commissions/+page.svelte
@@ -1,3 +1,19 @@
-<h1 align="center">
-    coming soon™
-</h1>
+<img src="commissions_banner.jpg" alt="faithful banner" class="banner-img" />
+
+<div class="padding-div">
+    <h1>coming soon™</h1>
+    <p>
+        lorum ipsum<br>
+        lorum ipsum<br>
+        lorum ipsum<br>
+        lorum ipsum<br>
+        lorum ipsum<br>
+        lorum ipsum<br>
+        lorum ipsum<br>
+        lorum ipsum<br>
+        lorum ipsum<br>
+        lorum ipsum<br>
+        lorum ipsum<br>
+        lorum ipsum<br>
+    </p>
+</div>
