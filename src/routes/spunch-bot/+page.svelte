b7ae6e45fe4e1daaedf5ce5bd01ddd53ba6c8e6d
Evorp <3vorpgaming@gmail.com>
2023-05-19 17:42:40 -0700

    spunch bot banner

--- src/routes/spunch-bot/+page.svelte
+++ src/routes/spunch-bot/+page.svelte
@@ -1,3 +1,19 @@
-<h1 align="center">
-    coming soon™
-</h1>
+<img src="spunch_bot_banner.jpg" alt="faithful banner" class="banner-img" />
+
+<div class="padding-div">
+    <h1>coming soon™</h1>
+    <p>
+        lorum ipsum<br>
+        lorum ipsum<br>
+        lorum ipsum<br>
+        lorum ipsum<br>
+        lorum ipsum<br>
+        lorum ipsum<br>
+        lorum ipsum<br>
+        lorum ipsum<br>
+        lorum ipsum<br>
+        lorum ipsum<br>
+        lorum ipsum<br>
+        lorum ipsum<br>
+    </p>
+</div>
