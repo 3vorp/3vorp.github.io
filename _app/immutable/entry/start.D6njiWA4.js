1c7ece40a3a37931572143e0f690d17cfae26127
Evorp <3vorpgaming@gmail.com>
2024-02-18 14:45:24 -0800

    Updates

---
+++ _app/immutable/entry/start.D6njiWA4.js
@@ -0,0 +1,1 @@
+import{a as t}from"../chunks/entry.CMFSgulw.js";export{t as start};
---
+++ _app/immutable/nodes/4.BwRUYe_H.js
@@ -0,0 +1,12 @@
+import{s as o,n as t}from"../chunks/scheduler.Dw93JyHj.js";import{S as u,i as l,e as n,c as a,g as p,b as c,d as b,f as d}from"../chunks/index.C4TWkI1A.js";function h(m){let r,e=`<h1>coming soon™</h1> <p>lorum ipsum<br/>
+        lorum ipsum<br/>
+        lorum ipsum<br/>
+        lorum ipsum<br/>
+        lorum ipsum<br/>
+        lorum ipsum<br/>
+        lorum ipsum<br/>
+        lorum ipsum<br/>
+        lorum ipsum<br/>
+        lorum ipsum<br/>
+        lorum ipsum<br/>
+        lorum ipsum<br/></p>`;return{c(){r=n("div"),r.innerHTML=e,this.h()},l(s){r=a(s,"DIV",{class:!0,"data-svelte-h":!0}),p(r)!=="svelte-qlnkiu"&&(r.innerHTML=e),this.h()},h(){c(r,"class","padding-div")},m(s,i){b(s,r,i)},p:t,i:t,o:t,d(s){s&&d(r)}}}class _ extends u{constructor(r){super(),l(this,r,null,h,o,{})}}export{_ as component};
