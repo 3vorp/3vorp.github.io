e0db3a18706611be45eb2d259a679d44ff6cff6b
Evorp <3vorpgaming@gmail.com>
2024-10-19 16:03:08 -0700

    fix wrapping

--- src/routes/commissions/+page.svelte
+++ src/routes/commissions/+page.svelte
@@ -1,10 +1,10 @@
 <script lang="ts">
 	const images: CommissionImage[] = [
 		{ src: "skull.gif", alt: "skull" },
+		{ src: "cat.gif", alt: "cat" },
 		{ src: "computer.png", alt: "computer" },
 		{ src: "dragon.png", alt: "dragon" },
 		{ src: "zombie.png", alt: "zombie" },
-		{ src: "cat.gif", alt: "cat" },
 	];
 </script>
 
@@ -14,9 +14,11 @@
 	<h1 align="center">Past Commissions</h1>
 	<div class="img-container">
 		{#each images as image}
-			<div class="img-cell">
-				<img src={`commission/${image.src}`} alt={image.alt || "commission image"} />
-			</div>
+			<img
+				class="comm-img"
+				src={`commission/${image.src}`}
+				alt={image.alt || "commission image"}
+			/>
 		{/each}
 	</div>
 </div>
@@ -24,8 +26,12 @@
 <style lang="scss">
 	.img-container {
 		display: flex;
-		flex-grow: 3;
-		justify-content: space-between;
-		flex-wrap: wrap;
+		flex-flow: row wrap;
+		align-content: center;
+		justify-content: center;
+	}
+	.comm-img {
+		max-height: 20em;
+		padding: 1em;
 	}
 </style>
