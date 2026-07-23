245203aa56651e1159aa83d0b60a6c32c81fa16a
Evorp <75297863+3vorp@users.noreply.github.com>
2026-07-23 16:35:58 -0700

    chore: update dependencies, move svelte config
    apparently it now goes in the vite config lol

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,17 @@
 import { sveltekit } from "@sveltejs/kit/vite";
+import { vitePreprocess } from "@sveltejs/vite-plugin-svelte";
+import adapter from "@sveltejs/adapter-static";
+
 import type { UserConfig } from "vite";
 
 export default {
-	plugins: [sveltekit()],
+	plugins: [
+		sveltekit({
+			preprocess: vitePreprocess({ script: true }),
+			adapter: adapter(),
+			alias: {
+				"~": "./src",
+			},
+		}),
+	],
 } as UserConfig;
